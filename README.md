9a8b5a46c047b706eb3cb3f274e9c00c44bc6935
physical-reasoning-project <3661914023@qq.com>
2023-10-02 08:07:44 +0800

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# SOPHY
+SoftPhy:
+Soft-Body Physical Concept Learning and Reasoning from Videos
+
+This is the repository that contains source code for the [SOPHY website](https://physical-reasoning-project.github.io).
---
+++ main.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("back-to-top");
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 200) {
+            btn.style.display = "block";
+            btn.style.opacity = 1;
+        } else {
+            btn.style.display = "none";
+            btn.style.opacity = 0;
+        }
+    });
+
+    btn.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
+
+const videoContainer = document.querySelector('.video-container');
+const content = document.querySelector('.content');
+
+window.addEventListener('scroll', function() {
+    // Define the height region where the video should start and stop being fixed
+    const startFixedPosition = content.offsetTop + 100; // Adjust as needed
+    const endFixedPosition = content.offsetTop + content.offsetHeight - 300; // Adjust as needed
+
+    if (window.scrollY > startFixedPosition && window.scrollY < endFixedPosition) {
+        videoContainer.classList.add('fixed-video');
+    } else {
+        videoContainer.classList.remove('fixed-video');
+    }
+});
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let video1 = document.getElementById("video1");
+//     video1.addEventListener("click", function() {
+//       window.location.hash = "content1";
+//     });
+  
+//     let video2 = document.getElementById("video2");
+//     video2.addEventListener("click", function() {
+//       window.location.hash = "content2";
+//     });
+  
+//     // ... repeat for other videos
+//   });
+
+
+// var e_count = 3; // set number of examples included;
+
+// function showexample(n) {
+
+//     for (let i = 1; i < e_count + 1; i++) {
+//         if (n == i) {
+//             document.getElementById("example" + i).style.display = "flex";
+//             document.getElementById("show" + i).className = "active";
+//         } else {
+//             document.getElementById("example" + i).style.display = "none";
+//             document.getElementById("show" + i).className = "";
+//         }
+//     }
+// };
+
+// showexample(1);
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const gifs = document.querySelectorAll('img');
+
+//     gifs.forEach(gif => {
+//         gif.addEventListener('click', function(event) {
+//             // Remove 'active' class from all content sections
+//             document.querySelectorAll('.content-section').forEach(section => {
+//                 section.classList.remove('active');
+//             });
+
+//             // Find the target content section using the href of the clicked gif's parent <a> tag
+//             const targetSectionId = gif.parentElement.getAttribute('href');
+//             const targetSection = document.querySelector(targetSectionId);
+
+//             // Add 'active' class to the target content section
+//             if (targetSection) {
+//                 targetSection.classList.add('active');
+//             }
+//         });
+//     });
+// });
